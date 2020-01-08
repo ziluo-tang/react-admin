0e10d76383b24bb1b17def2c8692e9bc6fee641a
tangxiaoxin <1302947749@qq.com>
2020-01-08 18:00:05 +0800

    登录页设计

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,10 +1,61 @@
 import React, { Component } from 'react';
-export default class Login extends Component{
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import { withRouter } from 'react-router-dom';
+import './index.less';
+class Login extends Component{
+    handleSubmit= e => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                this.props.history.push('/admin/home');
+            }
+        });
+    }
     render() {
+        const { getFieldDecorator } = this.props.form;
         return (
-            <div>
-                login page.
+            <div className="login">
+                <div className="login-logo">
+                    <img src="/assets/logo.svg" alt="logo"/>
+                    <span>REACT ADMIN</span>
+                </div>
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Item>
+                    {getFieldDecorator('username', {
+                        rules: [{ required: true, message: '请输入用户名' }],
+                    })(
+                        <Input
+                        prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                        placeholder="用户名"
+                        />,
+                    )}
+                    </Form.Item>
+                    <Form.Item>
+                    {getFieldDecorator('password', {
+                        rules: [{ required: true, message: '请输入密码' }],
+                    })(
+                        <Input
+                        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                        type="password"
+                        placeholder="密码"
+                        />,
+                    )}
+                    </Form.Item>
+                    <Form.Item>
+                    {getFieldDecorator('remember', {
+                            valuePropName: 'checked',
+                            initialValue: true,
+                        })(<Checkbox>记住密码</Checkbox>)}
+                        <a className="login-form-forgot" href="##">
+                            忘记密码
+                        </a>
+                        <Button type="primary" htmlType="submit" className="login-form-button">
+                            登录
+                        </Button>
+                    </Form.Item>
+                </Form>
             </div>
         );
     }
 }
+export default Form.create({ name: 'login-form' })(withRouter(Login))

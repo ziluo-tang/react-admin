fdbb03af70f381c8a1144c71d1eae4d0831893c0
tangxiaoxin <1302947749@qq.com>
2020-01-09 20:55:32 +0800

    baidumap

--- src/pages/map/index.jsx
+++ src/pages/map/index.jsx
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
+import './index.less';
 export default class CMap extends Component{
+    componentDidMount() {
+        this.renderMap();
+    }
+    renderMap = () => {
+        this.map = new window.BMap.Map("map-container"); 
+        let point = new window.BMap.Point(116.404, 39.915);  // 创建点坐标  
+        this.map.centerAndZoom(point, 15);
+        this.map.enableScrollWheelZoom(true);
+    }
     render() {
         return (
-            <div>map page.</div>
+            <div id="map-container" className="map-container"></div>
         )
     }
 }

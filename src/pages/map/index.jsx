3cdb4842e796f9a7588facd07704ad61e9344255
tangxiaoxin <1302947749@qq.com>
2024-03-15 17:05:42 +0800

    docker build

--- src/pages/map/index.jsx
+++ src/pages/map/index.jsx
@@ -1,18 +1,32 @@
-import React, { Component } from 'react';
-import './index.less';
-export default class CMap extends Component{
-    componentDidMount() {
-        this.renderMap();
+import React, { Component } from "react";
+import "./index.less";
+export default class CMap extends Component {
+  componentDidMount() {
+    this.renderMap();
+  }
+
+  getPosition = () => {
+    const position = {
+      x: 116.404,
+      y: 39.915,
+    };
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((p) => {
+        position.x = p.coords.longitude;
+        position.y = p.coords.latitude;
+      });
     }
-    renderMap = () => {
-        this.map = new window.BMap.Map("map-container"); 
-        let point = new window.BMap.Point(116.404, 39.915);  // 创建点坐标  
-        this.map.centerAndZoom(point, 15);
-        this.map.enableScrollWheelZoom(true);
-    }
-    render() {
-        return (
-            <div id="map-container" className="map-container"></div>
-        )
-    }
+    return position;
+  };
+
+  renderMap = () => {
+    this.map = new window.BMap.Map("map-container");
+    const { x, y } = this.getPosition();
+    let point = new window.BMap.Point(x, y); // 创建点坐标
+    this.map.centerAndZoom(point, 15);
+    this.map.enableScrollWheelZoom(true);
+  };
+  render() {
+    return <div id="map-container" className="map-container"></div>;
+  }
 }
